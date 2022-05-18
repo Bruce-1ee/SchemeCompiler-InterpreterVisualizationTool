c74c293efe23c1fc5e07ff22270d977cad5eeb4c
Bruce1ee <jxylzh@gmail.com>
2022-05-18 20:10:01 +0900

    5.18 主要修改了页面的布局

--- Visualization/drawLib.js
+++ Visualization/drawLib.js
@@ -1,40 +1,55 @@
-function makeNewElement(tag,content){
+/**
+ * 新建一个标签
+ * @param {String} tag 标签的类型
+ * @param {String} content 标签的内容
+ * @returns 
+ */
+function makeNewElement(tag, content) {
     let newElement = document.createElement(tag);
     let newContent = document.createTextNode(content);
     newElement.appendChild(newContent);
     return newElement;
 }
 
-function setAttribute(element,attribute,value){
-    element.setAttribute(attribute,value);
+/**
+ * 为一个element设置属性
+ * @param {Node} element 
+ * @param {String} attribute 
+ * @param {String} value 
+ */
+function setAttribute(element, attribute, value) {
+    element.setAttribute(attribute, value);
 }
 
 /**
  * 在一个节点内部加入一个新的元素
+ * @param {Node} currentNode 
+ * @param {Node} element 
  */
-function addElementInNode(currentNode,element){
+function addElementInNode(currentNode, element) {
     //let node = document.getElementById(currentNodeId);
     currentNode.appendChild(element);
 }
 
 /**
+ * 创建一个新的环境框架
  * @param {number} num 表示当前frame的编号，用于同步显示
  * @param {Array} args 装有这个frame中所有参数的表，需要将其绘制
  */
-function makeNewEnvironmentFrame(num,args){
+function makeNewEnvironmentFrame(num, args) {
     let currentNode = document.getElementById("environmentBox");
 
-    let newFrame = makeNewElement("div","");
-    for(var i = 0; i < args.length; i++){
-        let newArgument = makeNewElement("div",args[i]);
-        setAttribute(newArgument,"id","localEnvironmentFrameArgument_"+i);
-        setAttribute(newArgument,"class","localEnvironmentFrameArgument");
-        addElementInNode(newFrame,newArgument);
+    let newFrame = makeNewElement("div", "");
+    for (var i = 0; i < args.length; i++) {
+        let newArgument = makeNewElement("div", args[i]);
+        setAttribute(newArgument, "id", "localEnvironmentFrameArgument_" + i);
+        setAttribute(newArgument, "class", "localEnvironmentFrameArgument");
+        addElementInNode(newFrame, newArgument);
     }
-    setAttribute(newFrame,"id","localEnvironmentFrame_"+num);
-    setAttribute(newFrame,"class","localEnvironmentFrame");
+    setAttribute(newFrame, "id", "localEnvironmentFrame_" + num);
+    setAttribute(newFrame, "class", "localEnvironmentFrame");
 
-    addElementInNode(currentNode,newFrame);
+    addElementInNode(currentNode, newFrame);
 }
 
 /**
@@ -43,36 +58,48 @@ function makeNewEnvironmentFrame(num,args){
  * @param {number} stackCounter 
  * @param {String} content 
  */
-function makeNewStackFrame(callFrameNumber,stackCounter,content){
-    let currentNode = document.getElementById("callFrame_"+callFrameNumber);
+function makeNewStackFrame(callFrameNumber, stackCounter, content) {
+    let currentNode = document.getElementById("callFrame_" + callFrameNumber);
 
-    let newStackFrame = makeNewElement("div","");
-    setAttribute(newStackFrame,"id","stackFrame_"+stackCounter);
-    setAttribute(newStackFrame,"class","stackFrame");
+    let newStackFrame = makeNewElement("div", "");
+    setAttribute(newStackFrame, "id", "stackFrame_" + stackCounter);
+    setAttribute(newStackFrame, "class", "stackFrame");
 
-    let newStack = makeNewElement("div","");
+    let newStack = makeNewElement("div", "");
 
-    let newStackCounter = makeNewElement("div",stackCounter);
-    setAttribute(newStackCounter,"id","stackFrameConter_"+stackCounter);
-    setAttribute(newStackCounter,"class","stackFrameConter");
-    let newStackContent = makeNewElement("div",content);
-    setAttribute(newStackContent,"id","stackFrameContent_"+stackCounter);
-    setAttribute(newStackContent,"class","stackFrameContent");
+    let newStackCounter = makeNewElement("div", stackCounter);
+    setAttribute(newStackCounter, "id", "stackFrameConter_" + stackCounter);
+    setAttribute(newStackCounter, "class", "stackFrameConter");
+    let newStackContent = makeNewElement("div", content);
+    setAttribute(newStackContent, "id", "stackFrameContent_" + stackCounter);
+    setAttribute(newStackContent, "class", "stackFrameContent");
 
-    addElementInNode(newStack,newStackCounter);
-    addElementInNode(newStack,newStackContent);
+    addElementInNode(newStack, newStackCounter);
+    addElementInNode(newStack, newStackContent);
 
-    addElementInNode(newStackFrame,newStack);
+    addElementInNode(newStackFrame, newStack);
 
-    addElementInNode(currentNode,newStackFrame);
+    addElementInNode(currentNode, newStackFrame);
 }
 
-function makeNewCallFrame(callFrameNumber){
+function makeNewCallFrame(callFrameNumber) {
     let currentNode = document.getElementById("stackBox");
 
-    let newFrame = makeNewElement("div","");
-    setAttribute(newFrame,"id","callFrame_"+callFrameNumber);
-    setAttribute(newFrame,"class","callFrame");
+    let newFrame = makeNewElement("div", "");
+    setAttribute(newFrame, "id", "callFrame_" + callFrameNumber);
+    setAttribute(newFrame, "class", "callFrame");
 
-    addElementInNode(currentNode,newFrame);
+    addElementInNode(currentNode, newFrame);
+}
+
+function deleteStackFrame(callFrameNumber, stackCounter) {
+    let parentNode = document.getElementById("callFrame_" + callFrameNumber);
+    let currentNode = document.getElementById("stackFrame_" + stackCounter);
+    parentNode.removeChild(currentNode);
+}
+
+function deleteCallFrame(callFrameNumber) {
+    let parentNode = document.getElementById("stackBox");
+    let currentNode = document.getElementById("callFrame_" + callFrameNumber);
+    parentNode.removeChild(currentNode);
 }

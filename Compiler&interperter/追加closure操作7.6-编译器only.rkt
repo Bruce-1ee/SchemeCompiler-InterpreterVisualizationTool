c5e10a75a8a553f8a1e41d10e53aa710c74eacca
Bruce1ee <jxylzh@gmail.com>
2022-07-27 21:46:13 +0900

    7.27

--- Compiler&interperter/追加closure操作7.6-编译器only.rkt
+++ Compiler&interperter/追加closure操作7.6-编译器only.rkt
@@ -1,6 +1,8 @@
 #lang scheme
 (require r5rs)
+(require racket/trace)
 
+(define SS 30)
 (define closure-counter 0)
 (define free-v '())
 
@@ -59,6 +61,7 @@
   (compile test env free (list 'test then else)))
     
 (define (compile-lambda exp env next)
+  
   (let* ((vars (cadr exp))
          (body (caddr exp))
          (free-vars (find-free exp env))
@@ -71,11 +74,12 @@
                          (list 'return (+ (length vars) 1)))
                 next)))
 
-    (collect-free free-vars env ret))
+    (make-closure2 env free-vars ret)))
+  ;  (collect-free vars free-vars env ret))
     ;ret)
 ;{functional {refer-global a {return 1}} {halt}}
-
-    )
+;(trace compile-lambda)
+    
 (define (compile-application exp env free next)
   (letrec ((loop (lambda (args c)
                          (if (null? args)
@@ -112,12 +116,33 @@
 ;------新加入--------
 
 (define collect-free
-  (lambda (vars e next)
-    (if (null? vars)
+  (lambda (vars frees e next)
+    (if (null? frees)
         next
-        (collect-free (cdr vars) e
-                      (make-closure (car vars) e closure-counter
+        (collect-free (cdr frees) e
+                      (make-closure (car frees) e closure-counter
                                         next)))))
+
+(define (make-closure2 env frees next)
+
+  (letrec ((loop
+            (lambda (frees)
+              
+              (if (null? frees)
+                  next
+                  (list 'make-closure
+                        (get-index (car frees) (car env) 0)
+                        (loop (cdr frees)))))))
+    (if (null? env)
+        next
+        (loop frees))))
+
+  
+(define (get-index var vars i)
+  (cond
+    [(null? vars) 'error]
+    [(eq? var (car vars)) i]
+    [else (get-index var (cdr vars) (+ i 1))]))
 
 
 (define (make-closure var e n next) ;n : closure-number
@@ -342,4 +367,182 @@
 
 (define (sc x)
   (compile x '() '() '(halt)))
+
+
+
+
+;==========VM==========
+
+
+(define VM
+
+  (lambda (a x e c s)
+    (display "a:")(display a)(newline)
+    (display "x:")(display (car x))(newline)
+    (display "e:")(display e)(newline)
+    (display "c:")(display c)(newline)
+    (display "s:")(display s)(newline)
+    (display "stack:")(display stack)(newline)(newline)
+    (case (car x)
+      [(halt)  (vm-halt a x e c s)]
+      [(empty) (vm-empty a x e c s)]
+      [(refer-loacl) (vm-refer-loacl a x e c s)]
+      [(refer-global) (vm-refer-global a x e c s)]
+      [(refer-closure) (vm-refer-closure a x e c s)]
+      [(constant) (vm-constant a x e c s)]
+      [(close) (vm-close a x e c s)]
+      [(make-closure) (vm-make-closure a x e c s)]
+      [(functional) (vm-functional a x e c s)]
+      [(test) (vm-test a x e c s)]
+      [(frame) (vm-frame a x e c s)]
+      [(argument) (vm-argument a x e c s)]
+      [(apply) (vm-apply a x e c s)]
+      [(return) (vm-return a x e c s)]
+      [else (vm-else a x e c s)])))
+
+(trace VM)
+
+(define (vm-halt a x e c s) a)
+(define (vm-empty a x e c s) 'ok)
+
+(define (vm-refer-loacl a x e c s)
+  (let ((n (cadr x))
+        (m (caddr x))
+        (x (cadddr x)))
+    (VM (index (find-link n e) m) x e c s)))
+
+(define (vm-refer-global a x e c s)
+  (let ((var (cadr x))
+        (x (caddr x)))
+    (VM (find-global var GE) x e c s)))
+
+(define (vm-constant a x e c s)
+  (let ((obj (cadr x))
+        (x (caddr x)))
+    (VM obj x e c s)))
+
+;待修改
+(define (vm-close a x e c s) 
+  (let ((body (cadr x))
+        (next (caddr x)))
+    (if (null? c)
+        (VM (closure body e) next e c s)
+        (VM (closure body c) next e '() s))))
+
+(define (vm-functional a x e c s)
+  (let ((body (cadr x))
+        (x (caddr x)))
+    (VM (closure body e) x e c s)))
+
+(define (vm-test a x e c s)
+  (let ((then (cadr x))
+        (else (caddr x)))
+    (VM a (if a then else) e c s)))
+
+(define (vm-frame a x e c s)
+  (let ((ret (cadr x))
+        (x (caddr x)))
+    ;(display "frame") (newline)
+    ;(add-frame-counter)
+    (VM a x e c (push ret (push e s)))))
+
+(define (vm-argument a x e c s)
+  (if (pair? (car x))
+      (VM a (cadr x) e (push (cadr a) c s))
+      (VM a (cadr x) e c (push a s))))
+
+;未修改
+(define (vm-apply a x e c s)
+    (cond
+      [(eq? 'primitive (car a))
+       ;(display "apply") (newline)
+       ;(sub-frame-counter) ;自动删除
+       (letrec ((loop (lambda(argl n)
+                        (cond
+                          [(= n 0) (VM (apply (cdr a) argl)
+                                       (index (- s  (cadr x)) 0)
+                                       (index (- s  (cadr x)) 1)
+                                       (- (- s (cadr x)) 2))]
+                          [else  (loop (cons (index s (- n 1)) argl) (- n 1))]))))
+         (loop '() (cadr x)))]
+
+      [(eq? 'function (car a))
+       (if (eq? 'frame (car (cadr a)))
+           (VM a (cadr a) e c s)
+           (VM a (cadr (cadr a)) e c (push e s)))]
+      [else
+       (VM a (car a) s c (push (cadr a) s))]))
+
+(define (vm-return a x e c s)
+  ;(display "return") (newline)
+  ;(sub-frame-counter) ;自动删除
+  (let ([s (- s (cadr x))])
+         (VM a (index s 0) (index s 1) c (- s 2))))
+
+
+(define (vm-make-closure a x e c s)
+  (let ((i (cadr x))
+        (next (caddr x)))
+    (VM a
+        next
+        e
+        (append c (list (index e i)))
+        s)))
+ 
+(define (vm-refer-closure a x e c s)
+  (let ((i (cadr x))
+        (next (caddr x)))
+    (VM (index-closure c i)
+        next
+        e
+        c
+        s)))
+
+(define (index-closure clo i)
+  (if (eq? i 0)
+      (car clo)
+      (index-closure (cdr clo) (- i 1))))
+    
+
+
+
+;==========helping-function==========
+
+(define index ;找到s向下偏移i位置的元素
+  (lambda (s i)
+    (vector-ref stack (- (- s i) 1))))
+(define index-set!
+  (lambda (s i v)
+    (vector-set! stack (- (- s i) 1) v)))
+(define (find-link n e)
+  ;(display "n:   ")(display n)(newline)
+  ;(display "e:   ")(display e)(newline)
+  (if (= n 0) e
+      (find-link (- n 1) (index e -1))))
+(define closure
+  (lambda (body e)
+    (list body e)))
+
+
+(define (primitive-apply fun n s)
+  (letrec ( (loop (lambda (num argl)
+                    (if (= num 0)
+                        (apply fun argl)
+                        (loop (- 1 num) (index s num))))))
+    (loop n '())))
+
+
+
+
+(define stack (make-vector SS))
+(define (push x s)
+  (vector-set! stack s x)
+  (+ s 1))
+
+(define (vm-else a x e s)
+  (error "Unknow instruction -- VM"))
+
+(define run
+  (lambda (x)
+    (VM '() (compile x '() '() '(halt)) '() '()  0)))
 
